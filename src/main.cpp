7aa029a882f3b267d7a25a784fcce6c85faa3ca9
Carlos Federico <carlos.federico005@gmail.com>
2018-09-13 14:52:17 -0400

    Initial commit. Added OpenCV as library

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Hi!" << "\n";
+  std::cout << "OpenCV: " << CV_VERSION << "\n";
+  return 0;
+}
